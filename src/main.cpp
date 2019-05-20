9d03434a7b069e10c07826ce8760083fe0d0bdf9
Killian Blais <killian.blais@gmail.com>
2019-05-21 00:16:33 +0700

    board sucessfully connect to wifi

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,34 @@
-#include <Arduino.h>
-
+#include <ESP8266WiFi.h>
+// Network SSID
+const char* ssid = "Mike Homestay 4";
+const char* password = "vietnam1";
+ 
 void setup() {
-  // put your setup code here, to run once:
+  
+  Serial.begin(9600);
+  delay(10);
+ 
+  // Connect WiFi
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.hostname("Name");
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+ 
+  // Print the IP address
+  Serial.print("IP address: ");
+  Serial.print(WiFi.localIP());
 }
-
+ 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  
+//Add your project's loop code here
+  }
