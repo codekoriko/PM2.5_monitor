f6060a742b9d1f73db5e0f2df91dac3ff37038ff
psychonaute <killian.blais@gmail.com>
2024-01-21 23:27:49 +0700

    - publishing cmd format incorrect

---
+++ lib/publisher/gs_publisher.h
@@ -0,0 +1,94 @@
+#include "HTTPSRedirect.h"
+#include <ArduinoJson.h> // Include the ArduinoJson library
+#include <string>
+
+class GsPublisher
+{
+private:
+    Stream &serial;
+    const char* url;
+    const int httpsPort = 443; // Standard HTTPS port
+    const char *host = "script.google.com";          // Hostname for the HTTPS connection
+    HTTPSRedirect *client;
+
+public:
+    // Constructor
+    GsPublisher(const std::string &deploymentId, Stream &serialRef)
+        : serial(serialRef)
+    {
+        // Initialize HTTPSRedirect client
+        client = new HTTPSRedirect(httpsPort);
+        client->setInsecure();
+        client->setPrintResponseBody(true);
+        client->setContentTypeHeader("application/json");
+
+        // Log the connection attempt
+        serial.println("Connecting to ");
+        serial.println(host);
+        std::string urlString = "/macros/s/" + deploymentId + "/exec";
+        url = strdup(urlString.c_str());
+    }
+
+    // Destructor to clean up the HTTPSRedirect client
+    ~GsPublisher()
+    {
+        if (client != nullptr)
+        {
+            delete client;
+            client = nullptr;
+        }
+    }
+
+    // Attempt to connect with a specified number of retries
+    bool connect(int retries) {
+        if (client->connected()) {
+            serial.println("Client already connected.");
+            return true;  // Add this return statement
+        }
+
+        while (retries-- > 0) {
+            if (client->connect(host, httpsPort)) {
+                serial.println("Connected successfully.");
+                return true;
+            }
+            serial.println("Connection failed. Retrying...");
+            delay(1000); // Wait before retrying
+        }
+        serial.println("Failed to connect after all retries.");
+        return false;
+    }
+
+    // Publish method
+    bool publish(float temperature, int humidity, int firstData, int secondData) {
+        if (!client->connected()) {
+            if (!connect(3)) { // Attempt to connect with 3 retries
+                return false; // Connection failed
+            }
+        }
+
+        // Create a JSON object
+        StaticJsonDocument<200> doc;
+        doc["command"] = "append_row";
+        doc["sheet_name"] = "Sheet1";
+        JsonArray values = doc.createNestedArray("values");
+        values.add(temperature);
+        values.add(humidity);
+        values.add(firstData);
+        values.add(secondData);
+
+        // Serialize JSON object to a String
+        String jsonString;
+        serializeJson(doc, jsonString);
+        serial.println("jsonString:");
+        serial.println(jsonString);
+        // Send the JSON string to the server
+        // Assuming client->POST takes the JSON string and sends it to the server
+        if (client->POST(url, host, jsonString)) {
+            serial.println("Data published successfully.");
+            return true;
+        } else {
+            serial.println("Failed to publish data.");
+            return false;
+        }
+    }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,8 @@
 #include "HTTPSRedirect.h"
 #include <WiFiManager.h>
 
+#include <gs_publisher.h>
+
 #include <pms.h>
 #include <DHTesp.h>
 
@@ -17,23 +19,12 @@
 // #####################SETUP Gapp Script#####################
 // ###########################################################
 
-// Enter network credentials:
-// const char* ssid     = "TANG 8";
-// const char* password = "88888888";
-// const char *ssid = "Mike Homestay 4 2.4GHz";
-// const char *password = "vietnam1";
+const char *DEPLOYMENT_ID = "AKfycbxA5gwluyL3Nw2FK_tnUW2ix276BEaCVnsFYEc3JJIDYNHr_sQJUNh3bzOdgZyVIiI";
 
-// Enter Google Script Deployment ID:
-const char *GScriptId = "AKfycbxA5gwluyL3Nw2FK_tnUW2ix276BEaCVnsFYEc3JJIDYNHr_sQJUNh3bzOdgZyVIiI";
 // Create a JSON document
 StaticJsonDocument<200> doc;
 
-// Enter command (insert_row or append_row) and your Google Sheets sheet name (default is Sheet1):
-String payload_basev1 = "{\"command\": \"append_row\", \"sheet_name\": \"Sheet1\", \"values\": ";
-
-// Google Sheets setup (do not edit)
-const char *host = "script.google.com";
-String url = String("/macros/s/") + GScriptId + "/exec";
+GsPublisher* publisher;
 
 // columns name must match otherwise get (not helpful error) Error! Not connected to host.
 // Timestamp col get filled by the script
@@ -42,11 +33,8 @@ const char *columns[] = {
     "Temperature",
     "Humidity",
     "PM2.5",
-    "Meas. duration"};
-
-// set https
-const int httpsPort = 443;
-HTTPSRedirect *client = nullptr;
+    "Meas. duration"
+};
 
 // ###########################################################
 // #########################SETUP PMS#########################
@@ -201,37 +189,14 @@ void setup()
   WiFiManager wifiManager;
   wifiManager.autoConnect();
 
-  Serial.println("Setting up HTTPS connexion >>>");
+  publisher = new GsPublisher(DEPLOYMENT_ID, Serial); // Initialize the variable "publisher"
 
-  // Use HTTPSRedirect class to create a new TLS connection
-  client = new HTTPSRedirect(httpsPort);
-  client->setInsecure();
-  client->setPrintResponseBody(true);
-  client->setContentTypeHeader("application/json");
-
-  Serial.print("Connecting to ");
-  Serial.println(host);
-
-  // Try to connect for a maximum of 5 times
-  for (int i = 0; i < 5; i++)
-  {
-    if (client->connect(host, httpsPort))
-    {
-      Serial.print("Connected to ");
-      Serial.println(host);
-      break;
-    }
-    else
-      Serial.println((String) "Connection failed. Retrying " + i + "/5 ...");
-  }
-
-  delete client;    // delete HTTPSRedirect object
-  client = nullptr; // delete HTTPSRedirect object
 
   Serial.println(" Initializing Pms7003...");
   pms.begin();
   pms.waitForData(Pmsx003::wakeupTime);
   pms.write(Pmsx003::cmdModeActive);
+  pms.write(Pmsx003::cmdSleep);
 
   Serial.println("Initializing DHT11 on PIN D5...");
   dht.setup(DHT_IN, DHTesp::DHT11);
@@ -244,95 +209,31 @@ void setup()
 // ###########################################################
 // #########################LOOP##############################
 // ###########################################################
-bool measurement_sucessful = 0;
+
 void loop(void)
 {
-  // make sure our PM2.5 sensor sleep
-  pms.write(Pmsx003::cmdSleep);
-
-  if (client == nullptr)
-  {
-    client = new HTTPSRedirect(httpsPort);
-    client->setInsecure();
-    client->setPrintResponseBody(true);
-    client->setContentTypeHeader("application/json");
-  }
-  if (client == nullptr)
-  {
-    Serial.println("Error creating client object!");
-  }
-  else
-  {
-    if (!client->connected())
-    {
-      if (client->connect(host, httpsPort))
-      {
-        Serial.print("Connected to ");
-        Serial.println(host);
-      }
-      else
-      {
-        Serial.print("Could not connect to server: ");
-        Serial.println(host);
-        return;
-      }
-    }
-  }
-
   // ###get the measurements###
 
   // get pm2.5 measurement
   std::pair<int, int> measurement_data = getAveragePm2dot5(10);
+  int humidity = dht.getHumidity();
+  float temperature = dht.getTemperature();
   // std::pair<int, int> measurement_data;
   // measurement_data.first = 12;
   // measurement_data.second = 12;
 
-  // if has no measurement time, meausrement succeeded and we push gspread
-  if (measurement_data.second)
-  {
-    // get humidity and temperature from DHT11 sensor
-    int humidity = dht.getHumidity();
-    float temperature = dht.getTemperature();
-
-    char values_string[50];
-    snprintf(
-        values_string,
-        sizeof(values_string),
-        "%.1f,%d,%d,%d",
-        temperature,
-        humidity,
-        measurement_data.first,
-        measurement_data.second);
-    doc["values"] = values_string;
-
-    // Serialize the JSON document to a string
-    String payload;
-    serializeJson(doc, payload);
-
-    // Create json object string to send to Google Sheets
-    // payload = payload_basev1 + "\"" + measurement_data.first + "," + measurement_data.second + "\"}";
-
-    // Publish data to Google Sheets
-    Serial.println("Publishing data...");
-    Serial.println(payload);
-    if (client->POST(url, host, payload))
-    {
-      // do stuff here if publish was successful
-      Serial.println("Data successfully uploaded to spreadsheet");
+  // if has measurement time, meausrement succeeded and we push gspread
+  if (measurement_data.first != 0 && measurement_data.second != 0) {
+    if (publisher->publish(temperature, humidity, measurement_data.first, measurement_data.second)) {
+      Serial.println("Measurements published successfully.");
+    } else {
+      Serial.println("Failed to publish measurements.");
     }
-    else
-    {
-      // do stuff here if publish was not successful
-      Serial.println("Error while connecting");
-    }
-
     //  we wait measurement_freq before next loop
     Serial.println((String) "end of loop, Next loop in " + measurement_freq + "s");
     delay(measurement_freq * 1000);
-  }
-  else
-  {
-    // measurement fails we wait a bit before trying again
+  } else {
+    Serial.println("Measurement was unsuccessful, not publishing.");
     delay(5000);
   }
 }
